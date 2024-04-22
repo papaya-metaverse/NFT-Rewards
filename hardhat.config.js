e76ec5b21f34288b488a7bf07973521d392d5f6c
Valeriy Loshchenkov <codenamenoon@gmail.com>
2024-04-22 14:40:01 +0300

    Update env

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,25 +1,32 @@
 require('@nomicfoundation/hardhat-verify');
 require('@nomicfoundation/hardhat-chai-matchers');
-require("hardhat-contract-sizer");
 require('hardhat-dependency-compiler');
 require('hardhat-deploy');
-require('hardhat-gas-reporter');
 require('hardhat-tracer');
 require('dotenv').config();
 
+if(process.env.TEST) {
+  require("hardhat-contract-sizer");
+  require('hardhat-gas-reporter');
+}
+
 module.exports = {
     tracer: {
-        enableAllOpcodes: true,
+        // enableAllOpcodes: true,
     },
     solidity: {
-        version: '0.8.24',
-        settings: {
+      compilers: [
+        {
+          version: '0.8.24',
+          settings: {
             optimizer: {
-                enabled: true,
-                runs: 1_000_000,
+              enabled: true,
+              runs: 1_000_000,
             },
             viaIR: true,
+          },
         },
+      ]
     },
     namedAccounts: {
         deployer: {
@@ -38,16 +45,18 @@ module.exports = {
       // outputFile: "./gas-report",
       noColors: false
     },
-    dependencyCompiler: {
-        paths: [
-            '@1inch/solidity-utils/contracts/mocks/TokenCustomDecimalsMock.sol',
-        ],
-    },
+    // dependencyCompiler: {
+    //     paths: [
+    //         '@1inch/solidity-utils/contracts/mocks/TokenCustomDecimalsMock.sol',
+    //         '@1inch/solidity-utils/contracts/mocks/TokenMock.sol'
+    //     ],
+    // },
     etherscan: {
       apiKey:{
-        polygonMumbai: `${process.env.POLYGONSCAN_API_KEY}`,
-        polygon: `${process.env.POLYGONSCAN_API_KEY}`
-      }
+        polygon: `${process.env.POLYGONSCAN_API_KEY}` || '',
+        bsc: `${process.env.BSCSCAN_API_KEY}` || '',
+        bscTestnet: `${process.env.BSCSCAN_API_KEY}` || ''
+      },
     },
     defaultNetwork: "hardhat",
     namedAccounts: {
@@ -61,10 +70,10 @@ module.exports = {
          * blockGasLimit settings for different chains
          * For BSC: https://bscscan.com/chart/gaslimit
          * : 140000000
-         * 
+         *
          * For Polygon: https://forum.polygon.technology/t/increasing-gas-limit-to-30m/1652
          * : 30000000
-         * 
+         *
          * For Ethereum: https://ycharts.com/indicators/ethereum_average_gas_limit
          * : 30000000
          */
@@ -76,19 +85,27 @@ module.exports = {
           interval: 5000
         }
       },
-      mumbai: {
-        chainId: 80001,
-        url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_MUMBAI_KEY}`,
-        accounts: {
-          mnemonic: `${process.env.SEED_PHRASE_DEPLOYER}`,
-        }
-      },
       polygon: {
         chainId: 137,
         url: `https://polygon-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_POLYGON_KEY}`,
         accounts: {
           mnemonic: `${process.env.SEED_PHRASE_DEPLOYER}`,
         }
+      },
+      bsc: {
+        chainId: 56,
+        url: `https://bsc-dataseed.bnbchain.org/`,
+        accounts: {
+          mnemonic: `${process.env.SEED_PHRASE_DEPLOYER}`,
+        }
+      },
+      bscTestnet: {
+        chainId: 97,
+        url: `https://data-seed-prebsc-1-s1.bnbchain.org:8545`,
+        accounts: {
+          mnemonic: `${process.env.SEED_PHRASE_DEPLOYER}`,
+        },
+        gasPrice: 10000000000
       }
     },
 };
